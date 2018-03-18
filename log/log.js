0bdb667b99abe6b35739d3436e33203d371b5885
georgieff <lubo@georgieff.it>
2018-03-18 14:52:28 +0200

    let fix

--- log/log.js
+++ log/log.js
@@ -3,10 +3,10 @@ const path = require('path');
 const rfs = require('rotating-file-stream');
 
 // TODO REVAMP
-const dt = new Date();
-const month = ((dt.getMonth() < 9) ? '0' : '') + (dt.getMonth() + 1);
-const filename = `${month}-${dt.getDate()}-${dt.getFullYear()}.log`;
-const logDirectory = path.join(__dirname, '../logs/');
+let dt = new Date();
+let month = ((dt.getMonth() < 9) ? '0' : '') + (dt.getMonth() + 1);
+let filename = `${month}-${dt.getDate()}-${dt.getFullYear()}.log`;
+let logDirectory = path.join(__dirname, '../logs/');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 const accessLogStream = rfs(filename, {
     interval: '1d', // rotate daily
